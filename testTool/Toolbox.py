da127d79e4cde6d59b4eed8e4f6515edcf3f5c34
nitaichuchong <67508745+nitaichuchong@users.noreply.github.com>
2024-07-31 21:18:34 +0800

    完善了测试工具箱的测试数据生成功能，现在真正可用了

--- testTool/Toolbox.py
+++ testTool/Toolbox.py
@@ -22,7 +22,7 @@ class Toolbox_Window(QtWidgets.QMainWindow):
         self.con = sqlite3.connect(DATABASE_ROOT)
         self.cur = self.con.cursor()
         self.create_table()
-        self.init_comboBox()
+        self.init_comboBox_delete()
         # 初始化测试数据的时间范围，默认为当前日期到十天后
         self.ui.dateTimeEdit_start.setDateTime(datetime.now())
         self.ui.dateTimeEdit_end.setDateTime(datetime.now() + timedelta(days=10))
@@ -34,11 +34,14 @@ class Toolbox_Window(QtWidgets.QMainWindow):
 
     def init_slots(self):
         """绑定信号槽，点击按钮触发对应函数"""
+        # 连接功能模块
         self.ui.pushButton_generate.clicked.connect(self.create_test_data)
         self.ui.pushButton_update.clicked.connect(self.read_and_format)
         self.ui.pushButton_delete.clicked.connect(self.delete_table)
         self.ui.dateTimeEdit_start.dateTimeChanged.connect(self.dateTimeEdit_start_check)
         self.ui.dateTimeEdit_end.dateTimeChanged.connect(self.dateTimeEdit_end_check)
+        # 控件可用性控制
+        self.ui.checkBox.toggled.connect(self.ui.comboBox_random.setEnabled)
 
     def create_table(self):
         """创建表"""
@@ -65,6 +68,14 @@ class Toolbox_Window(QtWidgets.QMainWindow):
         date_start = self.ui.dateTimeEdit_start.dateTime()
         data_end = self.ui.dateTimeEdit_end.dateTime()
 
+        # 若该控件可用，表示开启随机中断功能，获取当前所选择的随机强度
+        random_num = None
+        if self.ui.comboBox_random.isEnabled():
+            random_num = int(self.ui.comboBox_random.currentText())
+            flag_random = True
+        else:
+            flag_random = False
+
         # 设置 i 作为日期增加的天数
         i = 0
         while True:
@@ -73,7 +84,13 @@ class Toolbox_Window(QtWidgets.QMainWindow):
             # 若当前时间的 date 已跟终止时间一致，说明满足退出循环的条件，结束生成
             if current_time.date() == data_end.date():
                 break
+            # 先将天数的控制 += 1，后续实际生成数据的操作往后放，以便随机中断的控制
             i += 1
+            # 若开启了随机中断，则随机范围根据随机强度的大小进行调整，以调整随机概率
+            if flag_random:
+                if random.randint(0, int(100 / random_num)) == 5:
+                    # 这表示跳过当天的数据生成
+                    continue
             # 转换为符合格式要求的 DateTime 对象，QDateTime 对象没有合适的方法
             current_time = current_time.toPyDateTime().strftime('%Y-%m-%d %H:%M:%S')
             # base64 只接受 byte 类型，因此将数据转换为 utf-8 字节串后进行 base64 编码
@@ -109,7 +126,7 @@ class Toolbox_Window(QtWidgets.QMainWindow):
         if not self.ui.textEdit.toPlainText().strip():
             QMessageBox.information(self, '操作提示', '当前表中还没有任何数据，先去生成吧')
 
-    def init_comboBox(self):
+    def init_comboBox_delete(self):
         """初始化 comboBox，将数据库中存在的表添加到 comboBox 选项中"""
         # sqlite_master 和 sqlite_sequence 都是 sqlite 自行创建的表，因此应当忽略
         # 从 sqlite_master 进行 SELECT 操作本身就忽略掉了 sqlite_master 表
@@ -120,7 +137,7 @@ class Toolbox_Window(QtWidgets.QMainWindow):
         # 列表生成式
         tables = [row[0] for row in res.fetchall()]
         for table in tables:
-            self.ui.comboBox.addItem(table)
+            self.ui.comboBox_delete.addItem(table)
 
     def dateTimeEdit_start_check(self):
         """检查起始时间是否符合要求"""
@@ -137,11 +154,11 @@ class Toolbox_Window(QtWidgets.QMainWindow):
     def delete_table(self):
         """删除指定表"""
         # 表名只能从控件 comboBox 的选项中选择
-        table_name = self.ui.comboBox.currentText()
+        table_name = self.ui.comboBox_delete.currentText()
         self.cur.execute(f"drop table if exists {table_name};")
         self.con.commit()
         # 完成一次删除后要更新 comboBox 的选项
-        self.init_comboBox()
+        self.init_comboBox_delete()
 
         QMessageBox.information(self, '操作提示', '你已删除指定表！（但实际上等同清空表）')
 
